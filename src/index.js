c89b2e6594dc31812911ee6d7223ecd71bd12b00
Matthew Dickey <matthewndickey@gmail.com>
2021-07-22 10:59:10 -0400

    Redux started

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     {" "}
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+    , document.getElementById("root")
+  </Provider>
 );
