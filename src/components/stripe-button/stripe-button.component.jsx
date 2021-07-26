f849e10c7717e7593aea62408dbd2cab048bfbb3
Matthew Dickey <matthewndickey@gmail.com>
2021-07-26 16:14:24 -0400

    updated Stripe integration, favicon, and page title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JHYhjGBDTpCFWd37i6hAB52XkGew1SaWVSK6FnCwb5aHcPTy2ahcMhQ9dXSO68mxzfmR95n7tCD6NbgkiDZxBu700SmiWB9EA";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="BlackTude"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
